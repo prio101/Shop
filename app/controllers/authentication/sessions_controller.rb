c3a367d5a7cc14e9aabe44b4034962988ded4b7e
prio101 <miprio101@gmail.com>
2021-03-27 15:12:51 +0600

    Added the login part of the user

---
+++ app/controllers/authentication/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Authentication::SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = 'Logged In'
+      redirect_to root_path
+    else
+      flash[:error] = 'Username or Password has mismatched or Incorrect'
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:success] = 'Successfully Logged Out'
+    redirect_to login_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
+
+  namespace :authentication do
+    resources :sessions, only: [ :new, :create, :destroy ]
+    get 'signup', to: 'sessions#signup'
+    post 'signup', to: 'sessions#create_user'
+    delete "logout", to: "sessions#destroy"
+  end
+
+
 end
