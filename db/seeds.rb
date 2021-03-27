64f333a361c5ce3c6104a02e23d4e2d8a0732a10
prio101 <miprio101@gmail.com>
2021-03-28 02:13:16 +0600

    added the seed for role and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  # creating admin
+  admin = User.create!(
+            username: 'admin',
+            password: 'password'
+          )
+
+  # Role Create
+  Role.create!(
+    name: 'admin',
+    summary: 'Admin Role',
+    user_id: admin.id 
+  )
+end
