87b7a06de6ab7ead76b4f0426b7d4938f95ce1df
prio101 <miprio101@gmail.com>
2021-03-27 22:25:57 +0600

    added the application helpers and also the navbar toggle of login / logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
 
+  resources :users
+
   namespace :authentication do
     resources :sessions, only: [ :new, :create, :destroy ]
     get 'signup', to: 'sessions#signup'
